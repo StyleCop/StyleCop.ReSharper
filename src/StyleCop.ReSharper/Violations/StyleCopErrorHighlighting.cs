1c3885c4a68c2862bb156ce5ed2b06b25e27fa01
Matt Ellis <m.t.ellis@gmail.com>
2016-08-26 20:08:37 +0100

    Blast. Missed a file way back in 87d7a71144c6c2355e28e8470e9257eccc6f2532

---
+++ src/StyleCop.ReSharper/Violations/StyleCopErrorHighlighting.cs
@@ -0,0 +1,31 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="StyleCopHighlighting.cs" company="http://stylecop.codeplex.com">
+//   MS-PL
+// </copyright>
+// <license>
+//   This source code is subject to terms and conditions of the Microsoft 
+//   Public License. A copy of the license can be found in the License.html 
+//   file at the root of this distribution. If you cannot locate the  
+//   Microsoft Public License, please send an email to dlr@microsoft.com. 
+//   By using this source code in any fashion, you are agreeing to be bound 
+//   by the terms of the Microsoft Public License. You must not remove this 
+//   notice, or any other, from this software.
+// </license>
+// <summary>
+//   Highlighting class for a StyleCop Violation set to severity level Error.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace StyleCop.ReSharper.Violations
+{
+    using JetBrains.DocumentModel;
+    using JetBrains.ReSharper.Feature.Services.Daemon;
+
+    [StaticSeverityHighlighting(Severity.ERROR, "StyleCop")]
+    public class StyleCopErrorHighlighting : StyleCopHighlightingBase
+    {
+        public StyleCopErrorHighlighting(ViolationEventArgs violationEventArgs, DocumentRange documentRange)
+            : base(violationEventArgs, documentRange)
+        {
+        }
+    }
+}
