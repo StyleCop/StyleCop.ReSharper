cd0a12dc832f2e10ce880d381a54643ea616d457
Matthias Koch <ithrowexceptions@gmail.com>
2021-11-16 01:07:33 +0100

    Fix installation with VS2022 present

--- settings.ps1
+++ settings.ps1
@@ -2,7 +2,7 @@ $PluginId = "StyleCop.StyleCop"
 $SolutionPath = "$PSScriptRoot\StyleCop.sln"
 $SourceBasePath = "$PSScriptRoot\src\dotnet"
 
-$VsWhereOutput = [xml] (& "$PSScriptRoot\tools\vswhere.exe" -format xml -latest -products *)
+$VsWhereOutput = [xml] (& "$PSScriptRoot\tools\vswhere.exe" -format xml -products *)
 $VisualStudio = $VsWhereOutput.instances.instance |
     Where-Object { $_.channelId -match "Release" } |
     Sort-Object -Property installationVersion |
